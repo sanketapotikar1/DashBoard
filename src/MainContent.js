a5d5768c38685792448f37e789e09329b1780ea9
Sanket Apotikar <sanketapotikar1@gmail.com>
2023-01-03 10:37:20 +0530

    changes

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,61 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Chart from "react-apexcharts";
+import { Chart } from "./Chart";
 
 export function MainContent() {
   return (
     <div>
-      <LineChart />
+      <Chart />
     </div>
   );
 }
 
-function LineChart() {
-  const [statechart, useStateChart] = useState({
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  });
 
-  const [pieChart, setPieChart] = useState({
-    options: {},
-    series: [44, 55, 41, 17, 15],
-    labels: ["A", "B", "C", "D", "E"],
-  });
-
-  return (
-    <div className="charts-container">
-      <div className="lineCharts-container"><Chart
-        options={statechart.options}
-        series={statechart.series}
-        type="bar"
-        width="575"
-      />
-      <Chart
-        options={statechart.options}
-        series={statechart.series}
-        type="line"
-        width="575"
-      /></div>
-      
-      <div className="donut">
-        <Chart
-          options={pieChart.options}
-          series={pieChart.series}
-          type="donut"
-          width="400"
-        />
-      </div>
-    </div>
-  );
-}
---
+++ src/chart.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+export function Chart() {
+  const [statechart, useStateChart] = useState({
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91],
+      },
+    ],
+  });
+
+  const [pieChart, setPieChart] = useState({
+    options: {},
+    series: [44, 55, 41, 17, 15],
+    labels: ["A", "B", "C", "D", "E"],
+  });
+
+  return (
+    <div className="charts-container">
+      <div className="lineCharts-container"><Chart
+        options={statechart.options}
+        series={statechart.series}
+        type="bar"
+        width="575" />
+        <Chart
+          options={statechart.options}
+          series={statechart.series}
+          type="line"
+          width="575" /></div>
+
+      <div className="donut">
+        <Chart
+          options={pieChart.options}
+          series={pieChart.series}
+          type="donut"
+          width="400" />
+      </div>
+    </div>
+  );
+}
